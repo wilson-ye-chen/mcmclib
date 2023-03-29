611af58a3e30f6d74a32ce968d866404da0af0f6
wilson-ye-chen <ye.chen@sydney.edu.au>
2023-03-29 16:35:52 +0100

    ADD: package boilerplate

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mcmclib',
+    version='0.1.0',
+    description='Library of MCMC sampling algorithms',
+    url='https://github.com/wilson-ye-chen/mcmclib',
+    author='Wilson Ye Chen',
+    license='MIT',
+    packages=['mcmclib'],
+    install_requires=['numpy', 'scipy']
+    )
