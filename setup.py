79e5d3892c0adf6b057a331b53094cb4826c100f
wilson-ye-chen <ye.chen@sydney.edu.au>
2023-04-04 16:51:53 +0100

    FIX: increment version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mcmclib',
-    version='0.1.0',
+    version='0.1.1',
     description='Library of MCMC sampling algorithms',
     url='https://github.com/wilson-ye-chen/mcmclib',
     author='Wilson Ye Chen',
